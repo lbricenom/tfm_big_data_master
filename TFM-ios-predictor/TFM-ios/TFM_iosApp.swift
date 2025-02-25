7fdcf1bc4407d1caef53b29c74f08c3a786db7ec
luis.briceno <luis.briceno@homeboost.com>
2025-02-25 08:35:41 -0500

    ios project

---
+++ TFM-ios-predictor/TFM-ios/TFM_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TFM_iosApp.swift
+//  TFM-ios
+//
+//  Created by luis on 16/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TFM_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TFM-ios-predictor/TFM-iosTests/TFM_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  TFM_iosTests.swift
+//  TFM-iosTests
+//
+//  Created by luis on 16/02/25.
+//
+
+import Testing
+@testable import TFM_ios
+
+struct TFM_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
