a569a42cb0bd52d43db0057c211f79f40440cf60
luis.briceno <luis.briceno@homeboost.com>
2025-02-25 08:22:35 -0500

    tfm entrega final.

---
+++ TFM-master-app/TFM-master-appTests/TFM_master_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  TFM_master_appTests.swift
+//  TFM-master-appTests
+//
+//  Created by home on 16/02/25.
+//
+
+import Testing
+
+struct TFM_master_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
